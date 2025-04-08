601ac86720e9e0a5ff83a587d4f9c392ce355fc4
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-04-08 13:32:15 +0200

    render multiple whunk through world class ok

--- TP/Scene/World.hpp
+++ TP/Scene/World.hpp
@@ -25,70 +25,9 @@ private:
 public:
     World() : SceneNode(Transform(), new MeshObject(), nullptr)
     {
-        int groundLevel = 4;
-        VoxelChunk chunk = VoxelChunk(CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE);
-        chunk.setBloc(2, groundLevel+1, 2, LOG_OAK);
-        chunk.setBloc(2, groundLevel+2, 2, LOG_OAK);
-        chunk.setBloc(2, groundLevel+3, 2, LOG_OAK);
-        chunk.setBloc(2, groundLevel+4, 2, LEAVES_OAK);
-        chunk.setBloc(2, groundLevel+3, 3, LEAVES_OAK);
-        chunk.setBloc(3, groundLevel+3, 2, LEAVES_OAK);
-        chunk.setBloc(2, groundLevel+3, 1, LEAVES_OAK);
-        chunk.setBloc(1, groundLevel+3, 2, LEAVES_OAK);
-        for (int i = 0; i < CHUNK_SIZE; i++) {
-            for (int j = 0; j < CHUNK_SIZE; j++) {
-                chunk.setBloc(i, groundLevel-4, j, BEDROCK);
-                chunk.setBloc(i, groundLevel-3, j, STONE);
-                chunk.setBloc(i, groundLevel-2, j, DIRT);
-                chunk.setBloc(i, groundLevel-1, j, DIRT);
-                chunk.setBloc(i, groundLevel, j, GRASS);
-            }
-        }
-        chunk.setBloc(8, groundLevel+4, 8, GLOWSTONE);
-        chunk.setBloc(8, groundLevel+4, 9, GLOWSTONE);
-        chunk.setBloc(9, groundLevel+4, 8, GLOWSTONE);
-        chunk.setBloc(9, groundLevel+4, 9, GLOWSTONE);
-        chunk.setBloc(8, groundLevel+5, 8, GLOWSTONE);
-        chunk.setBloc(8, groundLevel+5, 9, GLOWSTONE);
-        chunk.setBloc(9, groundLevel+5, 8, GLOWSTONE);
-        chunk.setBloc(9, groundLevel+5, 9, GLOWSTONE);
-        chunk.generateMesh();
-    
-        VoxelChunk chunk2 = VoxelChunk(CHUNK_SIZE, CHUNK_SIZE, CHUNK_SIZE);
-        chunk2.setBloc(2, groundLevel+1, 2, LOG_OAK);
-        chunk2.setBloc(2, groundLevel+2, 2, LOG_OAK);
-        chunk2.setBloc(2, groundLevel+3, 2, LOG_OAK);
-        chunk2.setBloc(2, groundLevel+4, 2, LEAVES_OAK);
-        chunk2.setBloc(2, groundLevel+3, 3, LEAVES_OAK);
-        chunk2.setBloc(3, groundLevel+3, 2, LEAVES_OAK);
-        chunk2.setBloc(2, groundLevel+3, 1, LEAVES_OAK);
-        chunk2.setBloc(1, groundLevel+3, 2, LEAVES_OAK);
-        for (int i = 0; i < CHUNK_SIZE; i++) {
-            for (int j = 0; j < CHUNK_SIZE; j++) {
-                chunk2.setBloc(i, groundLevel-4, j, BEDROCK);
-                chunk2.setBloc(i, groundLevel-3, j, STONE);
-                chunk2.setBloc(i, groundLevel-2, j, DIRT);
-                chunk2.setBloc(i, groundLevel-1, j, DIRT);
-                chunk2.setBloc(i, groundLevel, j, GRASS);
-            }
-        }
-        chunk2.setBloc(8, groundLevel+4, 8, GLOWSTONE);
-        chunk2.setBloc(8, groundLevel+4, 9, GLOWSTONE);
-        chunk2.setBloc(9, groundLevel+4, 8, GLOWSTONE);
-        chunk2.setBloc(9, groundLevel+4, 9, GLOWSTONE);
-        chunk2.setBloc(8, groundLevel+5, 8, GLOWSTONE);
-        chunk2.setBloc(8, groundLevel+5, 9, GLOWSTONE);
-        chunk2.setBloc(9, groundLevel+5, 8, GLOWSTONE);
-        chunk2.setBloc(9, groundLevel+5, 9, GLOWSTONE);
-        chunk2.generateMesh();
-
-        addChunk(0, 0, 0, &chunk);
-        addChunk(1, 0, 0, &chunk2);
+        
     }
     ~World() {
-        for (auto& [key, chunk] : chunks) {
-            delete chunk;
-        }
     }
     void addChunk(int x, int y, int z, VoxelChunk* chunk) {
         chunks[{x, y, z}] = chunk;
